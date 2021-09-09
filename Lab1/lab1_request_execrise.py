65138be92c33bc9a8624132b8665a7540361beb6
nmrchvz <npchavez@ualberta.ca>
2021-09-09 13:35:13 -0600

    Update lab1_request_execrise.py

--- Lab1/lab1_request_execrise.py
+++ Lab1/lab1_request_execrise.py
@@ -1,4 +1,4 @@
 import requests
 
-r = requests.get("http://google.com/")
+r = requests.get("https://raw.githubusercontent.com/nmrchvz/cmput404-labs/main/Lab1/lab1_request_execrise.py")
 print(r.text)
